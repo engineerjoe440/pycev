20ff002e0734c5afb8f0a9fdf87eac7961e8ce1a
Joe Stanley <engineerjoe440@yahoo.com>
2022-09-01 14:43:07 -0700

    added checksum test, corrected another issue

--- tests/functional/test_loadcev.py
+++ tests/functional/test_loadcev.py
@@ -7,7 +7,7 @@ import os
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent.parent))
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
 # Load the pycev Package
 import pycev
--- tests/functional/test_loadeverycev.py
+++ tests/functional/test_loadeverycev.py
@@ -8,7 +8,7 @@ import sys
 from pathlib import Path
 import pytest
 
-sys.path.insert(0, str(Path(__file__).parent.parent))
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
 # Load the pycev Package
 import pycev
