8b2a0ac0a9a2a8d0f7ffa83d7c4d36092e496d1b
sreekeshiyer <sreekesh.subramanian@gmail.com>
2022-09-02 21:58:09 +0530

    🐛 Updated setup.py | Change of Parameters in Setup

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/engineerjoe440/pycev",
-    packages=["pycev"],
+    py_modules=["pycev"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
