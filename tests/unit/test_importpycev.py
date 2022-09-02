4da78e4330cc8bec47abc390b298293b557b65fd
sreekeshiyer <sreekesh.subramanian@gmail.com>
2022-09-02 21:59:48 +0530

    🐛 Updated Unit Test

--- tests/unit/test_importpycev.py
+++ tests/unit/test_importpycev.py
@@ -8,20 +8,20 @@ from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
+
 def test_import_by_name():
     """Import Package Directly."""
     import pycev
 
-def test_import_as_package_module():
-    """Import Module from Package."""
-    from pycev import pycev
 
 def test_import_primary_class_pascal_case():
     """Import Class from Module - Lowercase."""
     from pycev import Cev
 
+
 def test_import_primary_class_uppercase():
     """Import Class from Module - Uppercase."""
     from pycev import CEV
+
 
 # END
